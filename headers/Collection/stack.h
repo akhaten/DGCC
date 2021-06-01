deba62841d0980d265a4de553b63838b76aacf52
khafif jessy <khafifjessy@gmail.com>
2021-06-01 10:51:08 +0200

    stack iterator implemented

--- headers/Collection/stack.h
+++ headers/Collection/stack.h
@@ -83,4 +83,76 @@ Stack stack_pop(Stack s);
  */
 void stack_map(Stack s, void* f(void *e));
 
+/**
+ * \struct Stack
+ * \brief Data structure for iterator of stack
+ * \note The iterator is not valid if there
+ * are mutations in the stack (push, pop...).
+ * Use stackiterator_reset() after each mutation or
+ * a set of mutations. If the stack is destroyed, the
+ * stack iterator must be destroyed.
+ */
+typedef struct s_StackIterator* StackIterator;
+
+/**
+ * \brief Create a stack iterartor
+ * \param[in] s : stack
+ * \return an iterator on the stack
+ * \pre (s != NULL)
+ */
+StackIterator stackiterator_new(Stack s);
+
+/**
+ * \brief Destroy the iterator
+ * \param[in,out] iter : stack iterator
+ * \pre (iter != NULL)
+ * \post (iter == NULL)
+ */
+void stackiterator_destruct(StackIterator iter);
+
+/**
+ * \brief Reset the iterator
+ * \param[in,out] iter : stack iterator
+ * \return stack iterator
+ * \pre (iter != NULL)
+ */
+StackIterator stackiterator_reset(StackIterator iter);
+
+/**
+ * \brief Check if there is a next element
+ * \param[in,out] iter : stack iterator
+ * \return true if the stack iterator can next
+ * \pre (iter != NULL)
+ * \warning The stack of iterator must be valid.
+ */
+bool stackiterator_hasnext(StackIterator iter);
+
+/**
+ * \brief Move stack iterator to next
+ * \param[in,out] iter : stack iterator
+ * \return stack iterator
+ * \pre (iter != NULL)
+ * \warning The stack of iterator must be valid.
+ */
+StackIterator stackiterator_next(StackIterator iter);
+
+/**
+ * \brief Move stack iterator to previous
+ * \param[in,out] iter : stack iterator
+ * \return stack iterator
+ * \pre (iter != NULL)
+ * \warning The stack of iterator must be valid.
+ */
+StackIterator stackiterator_previous(StackIterator iter);
+
+/**
+ * \brief Get the value of stack iterator
+ * \param[in] iter : stack iterator
+ * \return the value of stack iterator
+ * \pre (iter != NULL)
+ * \warning The stack of iterator must be valid.
+ * \note stackiterator_value(stackiterator_new(Stack s)) == NULL
+ */
+void *stackiterator_value(StackIterator iter);
+
 #endif
--- src/Collection/stack.c
+++ src/Collection/stack.c
@@ -3,13 +3,15 @@
 
 
 typedef struct s_Node {
-  void* value;
+  void *value;
+  struct s_Node *previous;
   struct s_Node *next;
 } *Node;
 
 struct s_Stack {
-  Node top;
+  Node sentinel;
   int size;
+  bool mutation;
 };
 
 
@@ -19,20 +21,69 @@ Stack stack_new(void){
   
   if(!s){
     perror("stack_new()");
-    exit(STACK_INIT);
+    exit(QUEUE_INIT);
   }
+
+  s->sentinel = malloc(sizeof(struct s_Node));
   
-  s->top = NULL;
+  if(!s->sentinel){
+    perror("stack_new()");
+    exit(SENTINEL_INIT);
+  }
+
+  s->sentinel->value = NULL;
+  s->sentinel->previous = s->sentinel;
+  s->sentinel->next = s->sentinel;
   s->size = 0;
+  s->mutation = true;
   
   return s;
 
 }
 
+void stack_destruct(Stack s){
+  
+  assert(s != NULL);
+
+  while(!stack_isEmpty(s))
+    stack_pop(s);
+
+  free(s->sentinel);
+  s->sentinel = NULL;
+  
+  free(s);
+  s = NULL;
+
+}
+
+int stack_size(const Stack s){
+  
+  assert(s != NULL);
+
+  return s->size;
+
+}
+
+bool stack_isEmpty(const Stack s){
+
+  assert(s != NULL);
+  
+  return !s->size;
+
+}
+
+void* stack_top(const Stack s){
+
+  assert( (s != NULL) && (!stack_isEmpty(s)) );
+
+  return s->sentinel->next->value;
+
+}
+
 Stack stack_push(Stack s, void *e){
-  
+
   assert( (s != NULL) && (e != NULL) );
-  
+
   Node new = malloc(sizeof(struct s_Node));
   
   if(!new){
@@ -40,70 +91,143 @@ Stack stack_push(Stack s, void *e){
     exit(ELEMENT_INIT);
   }
   
+  Node sentinel = s->sentinel;
   new->value = e;
-  new->next = s->top;
-  s->top = new;
+  new->previous = sentinel;
+  new->next = sentinel->next;
+  sentinel->next->previous = new;
+  sentinel->next = new;
   ++s->size;
+  s->mutation = true;
   
   return s;
 
 }
 
 Stack stack_pop(Stack s){
+
+  assert( (s != NULL) );
+
+  Node old = s->sentinel->next;
   
-  assert( (s != NULL) && (stack_isEmpty(s) == 0) );
+  old->previous->next = old->next;
+  old->next->previous = old->previous;
   
-  Node old = s->top;
-  
-  s->top = old->next;
   --s->size;
+  s->mutation = true;
+  
   free(old);
-  
+  old = NULL;
+
   return s;
-  
-}
-
-void* stack_top(const Stack s){
-  
-  assert( (s != NULL) && (stack_isEmpty(s) == 0) );
-  
-  return s->top->value;
-
-}
-
-int stack_size(const Stack s){
-  
-  assert( (s != NULL) );
-  
-  return s->size;
-
-}
-
-bool stack_isEmpty(const Stack s){
-
-  assert( (s != NULL) );
-  
-  return !s->size;
-
-}
-
-void stack_destruct(Stack s){
-
-  assert( (s != NULL) );
-  
-  while(!stack_isEmpty(s))
-    stack_pop(s);
-  
-  free(s);
 
 }
 
 void stack_map(Stack s, void* f(void *e)){
 
   assert( (s != NULL) );
-  
-  for(Node cur = s->top; cur != NULL; cur = cur->next)
+
+  for(Node cur = s->sentinel->next; cur != s->sentinel; cur = cur->next)
     cur->value = f(cur->value);
+  
+}
+
+struct s_StackIterator {
+  Node sentinel; 
+  Node cur;
+  bool *mutation;
+};
+
+StackIterator stackiterator_new(Stack s){
+  
+  assert( s != NULL );
+
+  StackIterator iter = malloc(sizeof(struct s_StackIterator));
+  
+  if(!iter){
+    perror("stackiterator_new()");
+    exit(ITERATOR_INIT);
+  }
+  
+  iter->sentinel = s->sentinel;
+  iter->cur = iter->sentinel;
+  s->mutation = false;
+  iter->mutation = &s->mutation;
+  
+
+  return iter;
+
+}
+
+void stackiterator_destruct(StackIterator iter){
+
+  assert( (iter != NULL) );
+  
+  free(iter);
+  iter = NULL;
+
+}
+
+bool stackiterator_mutation(StackIterator iter){
+  
+  assert( (iter != NULL) );
+  
+  return iter->mutation;
+
+}
+
+bool stackiterator_isValid(StackIterator iter){
+
+  assert( (iter != NULL) );
+  
+  return iter->sentinel != NULL;
+
+}
+
+StackIterator stackiterator_reset(StackIterator iter){
+  
+  assert( (iter != NULL) && (stackiterator_isValid(iter)) );
+  
+  iter->cur = iter->sentinel;
+  
+  return iter;
+
+}
+
+
+bool stackiterator_hasnext(StackIterator iter){
+  
+  assert( (iter != NULL) && (stackiterator_mutation(iter)) );
+  
+  return iter->cur->next != iter->sentinel;
+
+}
+
+StackIterator stackiterator_next(StackIterator iter){
+  
+  assert( (iter != NULL) && (stackiterator_mutation(iter)) );
+
+  iter->cur = iter->cur->next;
+
+  return iter;
+
+}
+
+StackIterator stackiterator_previous(StackIterator iter){
+
+  assert( (iter != NULL) && (stackiterator_mutation(iter)) );
+
+  iter->cur = iter->cur->previous;
+
+  return iter;
+
+}
+
+void *stackiterator_value(StackIterator iter){
+
+  assert( (iter != NULL) && (stackiterator_mutation(iter)) );
+
+  return iter->cur->value;
 
 }
 
